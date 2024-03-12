24e44ad130d9b814a31b03b2f70fb31411054a8d
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-03-12 03:10:02 +0100

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -7,3 +7,37 @@ View the bar chart and hover over the individual bars to see the correct amounts
 - **Bonus**: See dynamically generated bars based on the data provided in the local JSON file
 
 */
+// const file  = data.json
+const blueColor = "hsl(186, 34%, 60%)";
+const pinkColor = "hsl(10, 79%, 65%)";
+const xValues = ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"];
+const yValues = [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48];
+const barColors = [
+  pinkColor,
+  pinkColor,
+  blueColor,
+  pinkColor,
+  pinkColor,
+  pinkColor,
+  pinkColor,
+];
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
+      },
+    ],
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
+      // text: "World Wine Production 2018",
+    },
+  },
+});
